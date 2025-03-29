d031fa9c34a853725657f8f4d42532e26bca6823
Ivan Zheng <ivanzh@mit.edu>
2025-03-28 20:29:19 -0400

    Implement some jump and branch instructions in Process class

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# initialize the register names and symbolic names
+symbolic_names = ['zero','ra','sp','gp','tp','t0','t1','t2','s0','s1',
+                   'a0','a1','a2','a3','a4','a5','a6','a7',
+                   's2','s3','s4','s5','s6','s7','s8','s9','s10','s11',
+                   't3','t4','t5','t6']
+register_numbers = {}
+for i in range(32):
+    register_numbers[f'x{i}'] = i
+    register_numbers[symbolic_names[i]] = i
+
 class Instruction:
     def __init__(self, instr_type, arg1=None, arg2=None, arg3=None):
         self.instr_type = instr_type
@@ -30,6 +40,63 @@ class Memory:
     def display(self):
         # TODO: improve formatting later
         print(self.memory)
+
+class Process:
+    """
+    Represents a process, with 32 registers each 32 bits large.
+    Registers may be referred to by symbolic names, or just x0-x31.
+    Acts on a virtual memory space.
+    """
+    def __init__(self, instructions, labels, memory):
+        """
+        instructions is a list of Instructions,
+        labels is a dict mapping string label names to list indices in instructions.
+        memory is a Memory object that this Process will interact with.
+        """
+        self.registers = [0x0 for i in range(32)]
+        self.instructions = instructions
+        self.labels = labels
+        self.memory = memory
+        self.pc = 0x0
+
+    def execute(self, instr):
+        arg1_reg_num = register_numbers[instr.arg1] if instr.arg1 in register_numbers.keys() else 0
+        arg2_reg_num = register_numbers[instr.arg2] if instr.arg2 in register_numbers.keys() else 0
+        arg3_reg_num = register_numbers[instr.arg3] if instr.arg3 in register_numbers.keys() else 0
+        match instr.instr_type:
+            case 'lui':
+                self.registers[arg1_reg_num] = instr.arg2 << 12
+            case 'jal':
+                self.registers[arg1_reg_num] = self.pc + 0x4
+                self.pc = self.labels[instr.arg2]
+            case 'jalr':
+                self.registers[arg1_reg_num] = self.pc + 0x4
+                # TODO: parse offset(rs1) to update pc
+            case 'beq':
+                branch = (self.registers[arg1_reg_num] == self.registers[arg2_reg_num])
+                self.pc = self.labels[instr.arg3] * 4 if branch else self.pc + 0x4
+            case 'bne':
+                branch = (self.registers[arg1_reg_num] != self.registers[arg2_reg_num])
+                self.pc = self.labels[instr.arg3] * 4 if branch else self.pc + 0x4
+            case 'blt':
+                branch = (self.registers[arg1_reg_num] < self.registers[arg2_reg_num])
+                self.pc = self.labels[instr.arg3] * 4 if branch else self.pc + 0x4
+            case 'bge':
+                branch = (self.registers[arg1_reg_num] >= self.registers[arg2_reg_num])
+                self.pc = self.labels[instr.arg3] * 4 if branch else self.pc + 0x4
+            case 'bltu':
+                branch = (self.registers[arg1_reg_num] < self.registers[arg2_reg_num])
+                self.pc = self.labels[instr.arg3] * 4 if branch else self.pc + 0x4
+                # TODO: handle signed and unsigned operations, this and bgeu
+            case 'bgeu':
+                branch = (self.registers[arg1_reg_num] >= self.registers[arg2_reg_num])
+                self.pc = self.labels[instr.arg3] * 4 if branch else self.pc + 0x4
+
+
+
+
+
+
 
 def tokenize_with_line_grouping(filename):
     """
